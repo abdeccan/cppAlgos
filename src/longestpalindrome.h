efce34ef8c100d8e4dac8bf880c203ae074a72cf
abdeccan <bkrishnamurthy@intuit.com>
2020-02-02 17:34:03 +0530

    second set of changes for longest palindrome

--- src/longestpalindrome.h
+++ src/longestpalindrome.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "pch.h"
 
+#define _LONGESTPALINDROME_TEST_
+
+
 // https://leetcode.com/problems/longest-palindromic-substring/
 class LongestPalindromeSubstr {
 
 public:
-
+	string getmaxlen(string s, int left, int right) {
+		
+		while (s[right] == s[left]) {			
+			if (left == 0 || right > s.size())
+				break;
+			right++;
+			left--;
+		};
+		return s.substr(left, right- left + 1);
+	}
+	
 	string longestPalindrome(string s) {
 
 		int size = s.size();
@@ -22,25 +35,16 @@ public:
 
 		string max;
 		int maxsize = 0;
-		string tmp;
+		string tmp1, tmp2 ,tmp;
+		//int len = 0, len1 = 0, len2 = 0;
+		//int right = 0, left = 0;
 		for (int i = 1; i < size; i++) {
-			// pivot
-			for (int p = 1; p <= i; p++) {
-				if (s[i - p] == s[i + p] && (maxsize < 2 * p + 1)) {
-					max = s.substr(i - p, 2 * p + 1);
-					maxsize = max.size();
-					printf("\n max str found: %s with size: %d", max.c_str(), maxsize);
-					continue;
-				}
-				else if (/* s[i-p] == s[i+p+1] &&*/ s[p] == s[i + p] && (maxsize < 2 * p + 2)) {
-					max = s.substr(i - p, 2 * i + 2);
-					maxsize = max.size();
-					printf("\n max str found: %s with size: %d", max.c_str(), maxsize);
-					continue;
-				}
-				break;
-			}
+			tmp1 = getmaxlen(s, i, i);
+			tmp2 = getmaxlen(s, i, i + 1);
+			tmp = tmp1 > tmp2 ? tmp1 : tmp2;
 		}
+
+		max = tmp;
 
 
 		return max;
@@ -100,65 +104,6 @@ public:
 			}
 		}
 		// delete[] tmp;
-		return max;
-	}
-};
-class longestpalindrome {
-public:
-	inline char * reverse(char *s) {
-		if (s == nullptr) return s;
-
-		int size = strlen(s);
-		char *p1 = s;
-		char *p2 = s + size - 1;
-		while (p1 < p2) {
-			char t = *p1;
-			*p1++ = *p2;
-			*p2-- = t;
-		}
-		return s;
-	}
-
-	string longestPalindrome_brteuf(string s) {
-
-		int size = s.size();
-
-		if (size == 0)
-			return s;
-
-		if (size > 1000) {
-			s.clear();
-			return s;
-		}
-
-		string max;
-		int maxSize = 0;
-		char *tmp = new char[size + 1];
-		//memset(tmp, 0, size);
-		for (int i = 0; i < size; i++) {
-
-			for (int j = size; j > maxSize; j--) {
-
-				if (i + j > size) continue;
-
-				string tmp1 = s.substr(i, j);
-				//string tmp2(tmp1.rbegin(), tmp1.rend());
-				string tmp2 = tmp1;
-				std::reverse(tmp1.begin(), tmp1.end());
-			   // memset(tmp, 0, size+1);
-				//strcpy(tmp, tmp1.c_str());
-				//char *tmp2 = reverse(tmp);
-				//printf("\n\tafter reverse, tmp1 = %s, tmp2 = %s", tmp1.c_str(), tmp2);
-				//if ((strcmp(tmp1.c_str(), tmp2) == 0) && (max.size() < tmp1.size())) {
-				if (tmp1 == tmp2 && (max.size() < tmp1.size())) {
-					max = tmp1;
-					maxSize = max.size();
-					//printf("\nmax found so far: %s at i=%d, j =%d", max.c_str(), i, j);
-					break;
-				}
-			}
-		}
-		delete[] tmp;
 		return max;
 	}
 };
